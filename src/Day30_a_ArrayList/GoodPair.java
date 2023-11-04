6978b1bfb98b80b68a7d093ed599485466a46243
Emil <emilmovlayev1@gmail.com>
2023-11-04 15:14:58 -0400

    Class

---
+++ src/Day30_a_ArrayList/GoodPair.java
@@ -0,0 +1,46 @@
+package Day30_a_ArrayList;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class GoodPair {
+
+
+        //    public static void main(String[] args) {
+//        ArrayList <Integer> nums = new ArrayList<>(Arrays.asList(1, 3, 4, 5, 6, 7, 8));
+//                                                        // elements: 1, 3, 4, 5, 6, 7, 8 --- >  1, 3, 4, 6, 7, 8 -- >  1, 3, 4, 6, 8
+//                                                        // indexes:  0  1  2  3  4  5  6 --- >  0  1  2  3  4  5  6 - >0  1  2  3  4  5  6
+//        for (int i = 0; i < nums.size(); i++) {                 // i = 0    | i = 1   | i = 2   | i = 3 | i = 4 | i = 5
+//
+//            //System.out.println( "Each: " +  nums.get(i)  );
+//
+//            if (nums.get(i) > 4) {                              //  1 > 4   | 3 > 4   | 4 > 4   | 5 > 4  | 7 > 4
+//                nums.remove( (Integer)nums.get(i) );            //
+//            }
+//
+//
+//        }
+//
+//        System.out.println(nums);
+//
+//
+//    }
+
+        public static void main(String[] args) {
+            ArrayList <Integer> nums = new ArrayList<>(Arrays.asList(1, 3,
+                    8, 5,
+                    6, 7,
+                    18, 10));
+            ArrayList <Integer> nums2 = new ArrayList<>();
+
+            for (int i = 0; i < nums.size(); i++) {
+
+                if ( nums.get(i) >= 4 ) {
+                    nums2.add(nums.get(i));
+                }
+
+            }
+            System.out.println(nums);
+            System.out.println(nums2);
+
+        }
+
+}
---
+++ src/day30_b_Custom_classes/Person.java
@@ -0,0 +1,9 @@
+package day30_b_Custom_classes;
+
+public class Person {
+    // INSTANCE VARIABLES -  declared at class level
+    String name;
+    int age;
+    double height;
+    boolean isMarried;
+}
