2f85df526971c742ea254049fb3be416a359298b
Emil <emilmovlayev1@gmail.com>
2023-11-26 14:08:34 -0500

    Polymorphism

---
+++ src/day_39_polymorphism/clothes/Jacket.java
@@ -0,0 +1,16 @@
+package day_39_polymorphism.clothes;
+
+public class Jacket extends Clothes implements HasHood {
+
+    @Override
+    public void wear () {
+        System.out.println("Wearing a jacket");
+    }
+
+
+    @Override
+    public void putOnHood () {
+        System.out.println("Putting on the jacket hood");
+    }
+
+}
---
+++ src/day_39_polymorphism/clothes/TShirt.java
@@ -0,0 +1,10 @@
+package day_39_polymorphism.clothes;
+
+public class TShirt extends Clothes {
+
+    @Override
+    public void wear (){
+        System.out.println("Wearing a t-shirt");
+    }
+
+}
