9b081560745ca756ade5c0f81a7e8dc6de68e574
Emil <emilmovlayev1@gmail.com>
2023-12-06 22:08:29 -0500

    New Map

--- src/day39_b_exception/learn_exception/FourthTry.java
+++ src/day39_b_exception/learn_exception/FourthTry.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class FourthTry {
     public static void main(String[] args) {
 
-        public static void main(String[] args) {
+
 
             try {
                 System.out.println("Start");
@@ -19,7 +19,7 @@ public class FourthTry {
                 e.printStackTrace();
             }
 
-        }
+
 
     }
 }
